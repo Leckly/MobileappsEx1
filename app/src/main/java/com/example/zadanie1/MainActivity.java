c0ca2dc4437e667655849a1b455a7dc0168ece5e
Leckly <minejoyjoy@gmail.com>
2022-10-24 00:49:57 +0200

    Ex2

--- app/src/main/java/com/example/zadanie1/MainActivity.java
+++ app/src/main/java/com/example/zadanie1/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.zadanie1;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,10 +16,23 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String KEY_CURRENT_INDEX = "currentIndex";
+    public static final String KEY_EXTRA_ANSWER = "pl.edu.pb.wi.quiz.correctAnswer";
+    private static final int REQUEST_CODE_PROMPT = -1;
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+        Log.d("Tag","Wywołana została metoda: onSaveInstanceState");
+        outState.putInt(KEY_CURRENT_INDEX,currentIndex);
+    }
+
     private Button trueButton;
     private Button falseButton;
     private Button nextButton;
     private TextView questionTextView;
+    private Button promptButton;
 
     private Question[] questions = new Question[]{
             new Question(R.string.q_activity,true),
@@ -24,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
     };
 
     private int currentIndex = 0;
+    private boolean answerWasShown;
 
     private void checkAnswerCorectness(boolean userAnswer){
         boolean correctAnswer = questions[currentIndex].isTrueAnswer();
@@ -40,20 +60,59 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Tag","Wywołana została metoda cyklu życia: onDestroy");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Tag","Wywołana została metoda cyklu życia: onStop");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Tag","Wywołana została metoda cyklu życia: onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Tag","Wywołana została metoda cyklu życia: onPause");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Tag","Wywołana została metoda cyklu życia: onStart");
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("tag","Wywołana została metoda cyklu życia: onCreate");
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState != null)
+        {
+            currentIndex = savedInstanceState.getInt(KEY_CURRENT_INDEX);
+        }
 
         trueButton =findViewById(R.id.true_button);
         falseButton = findViewById(R.id.false_button);
         nextButton = findViewById(R.id.next_button);
         questionTextView = findViewById(R.id.question_text_view);
+        promptButton = findViewById(R.id.prompt_button);
 
 
         trueButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 checkAnswerCorectness(true);
+                currentIndex = (currentIndex + 1 )%questions.length;
+                setNextQuestion();
             }
         });
 
@@ -61,6 +120,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 checkAnswerCorectness(false);
+                currentIndex = (currentIndex + 1 )%questions.length;
+                setNextQuestion();
             }
         });
 
@@ -72,5 +133,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         setNextQuestion();
+        ActivityResultLauncher<Intent> startActivityForResult = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() != RESULT_OK) {
+                            return;
+                        }
+                        if (result.getResultCode() == REQUEST_CODE_PROMPT) {
+                            Intent data = result.getData();
+                            if (data == null) {
+                                return;
+                            }
+                            answerWasShown = data.getBooleanExtra(PromptActivity.KEY_EXTRA_ANSWER_SHOWN, false);
+                        }
+                    }
+                });
+
+        promptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PromptActivity.class);
+                boolean correctAnswer = questions[currentIndex].isTrueAnswer();
+                intent.putExtra(KEY_EXTRA_ANSWER, correctAnswer);
+                startActivityForResult.launch(intent);
+            }
+        });
     }
+
 }
